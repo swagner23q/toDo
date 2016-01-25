d781a849daf59f79e181eae67223a51791ade986
Rob King and Sara Wagner <samiamspop@gmail.com>
2016-01-25 14:25:47 -0800

    add delete option on list items

--- js/scripts.js
+++ js/scripts.js
@@ -18,14 +18,19 @@ $(document).ready(function() {
     var inputtedDate = $("input#newDate").val();
     var newTask = new Task(inputtedTask, inputtedDescription, inputtedDate);
 
-    $("ul#tasksCompleted").append("<li><span class='listTask'>" + newTask.toDo() + "</span></li>");
+    // $("ul#tasksCompleted").append("<li><span class='listTask' id='" + newTask.title + "'>" + newTask.toDo() + "</span></li><button type='delete' class='btn btn-delete'>delete</button>");
+    $("ul#tasksCompleted").append("<li><span class='listTask'>" + newTask.toDo() + "</span>  <span class='itemDelete'>Delete</span></li>");
+
+    $('#tasksCompleted').on('click', '.itemDelete', function() {
+        $(this).closest('li').remove();
+    });
 
     $("input#newTask").val("");
     $("input#newDescription").val("");
     $("input#newDate").val("");
 //
     $(".listTask").last().click(function() {
-      $(".showTasks").show();
+      $(".showTasks").toggle();
       $(".showTasks h2").text(newTask.toDo());
       $(".taskTitle").text(newTask.title);
       $(".description").text(newTask.description);
